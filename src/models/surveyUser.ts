50be93be3e63f8b1b3c904c4d8b268de871c566c
manununes <manununes@hotmail.com.br>
2021-02-27 00:33:47 -0300

    features: conteúdo da aula 04

--- src/models/surveyUser.ts
+++ src/models/surveyUser.ts
@@ -1,24 +1,36 @@
-import { Column, CreateDateColumn, Entity, PrimaryColumn } from "typeorm";
-import {v4 as uuid } from 'uuid'
+import { Column, CreateDateColumn, Entity, JoinColumn, ManyToOne, PrimaryColumn } from "typeorm";
+import { v4 as uuid } from 'uuid'
+import { Survey } from "./survey";
+import { User } from "./user";
 
 @Entity("surveys_users")
 class SurveyUser {
   @PrimaryColumn()
-  readonly id:string;
+  readonly id: string;
   @Column()
-  user_id:string;
+  user_id: string;
+
+  @ManyToOne(() => User)
+  @JoinColumn({ name: "user_id" })
+  "Usuário": User
+
   @Column()
-  survey_id:string;
+  survey_id: string;
+
+  @ManyToOne(()=> Survey)
+  @JoinColumn({name:"survey_id"})
+  "Pesquisa REF":Survey
+  
   @Column()
-  value:number;
+  value: number;
   @CreateDateColumn()
-  created_at:Date;
+  created_at: Date;
   //Cria uma chave UUID automaticamente quando o model é chamado pelo controller
-  constructor(){
-    if(!this.id){
+  constructor() {
+    if (!this.id) {
       this.id = uuid()
     }
   }
 }
 
-export {SurveyUser}
+export { SurveyUser }
--- src/services/sendMailService.ts
+++ src/services/sendMailService.ts
@@ -1,25 +1,40 @@
+import fs from 'fs'
+import handlebars from 'handlebars'
 import nodemailer, { Transporter } from 'nodemailer'
 
-class SendMailService{
+class SendMailService {
   private client: Transporter
-  constructor(){
-    nodemailer.createTestAccount().then(account=>{
+  constructor() {
+    nodemailer.createTestAccount().then(account => {
       const transporter = nodemailer.createTransport({
-        host:account.smtp.host,
+        host: account.smtp.host,
         port: account.smtp.port,
-        secure:account.smtp.secure,
-        auth:{
-          user:account.user,
-          pass:account.pass
+        secure: account.smtp.secure,
+        auth: {
+          user: account.user,
+          pass: account.pass
         }
       });
       this.client = transporter
     })
   }
-  
-  async execute(){
+
+  async execute(to: string, subject: string, variables: object,path:string) {
     
+    const templateFileContent = fs.readFileSync(path).toString("utf-8")
+    const mailtemplatePars = handlebars.compile(templateFileContent)
+
+    const html = mailtemplatePars(variables)
+
+    const message = await this.client.sendMail({
+      to,
+      subject,
+      html,
+      from: "NPS <noreplay@nps.com.br>"
+    })
+    console.log("messagem enviada: %s", message.messageId);
+    console.log("Preview URL %s", nodemailer.getTestMessageUrl(message))
   }
 }
 
-export {SendMailService}
+export default new SendMailService()
